31d36935d14921f12281995099838b1e2f8ec9db
shahpnmlab <34034134+shahpnmlab@users.noreply.github.com>
2024-08-10 11:05:26 +0100

    ???

--- WarpTools/Commands/Tiltseries/TemplateMatchTiltseries.cs
+++ WarpTools/Commands/Tiltseries/TemplateMatchTiltseries.cs
@@ -64,8 +64,8 @@ namespace WarpTools.Commands
         [Option("lowpass", Default = 1.0, HelpText = "Gaussian low-pass filter to be applied to template and tomogram, in fractions of Nyquist; 1.0 = no low-pass, <1.0 = low-pass")]
         public double Lowpass { get; set; }
 
-        [Option("full_covered_voxels", Default = true, HelpText = "Keep only fully covered voxels during matching")]
-        public bool FullCoveredVoxels { get; set; } = true;
+        [Option("full_covered_voxels", HelpText = "Keep only fully covered voxels during matching")]
+        public bool FullCoveredVoxels { get; set; }
 
         [Option("lowpass_sigma", Default = 0.1, HelpText = "Sigma (i.e. fall-off) of the Gaussian low-pass filter, in fractions of Nyquist; larger value = slower fall-off")]
         public double LowpassSigma { get; set; }
